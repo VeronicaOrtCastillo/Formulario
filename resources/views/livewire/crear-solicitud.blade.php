a6a9fa5713befd9f0e64bb943db05a769afa7037
VeronicaOrtCastillo <veronicaortcaz@gmail.com>
2024-12-09 12:26:22 -0600

    Se cambia leyenda de solicitud enviada

--- resources/views/livewire/crear-solicitud.blade.php
+++ resources/views/livewire/crear-solicitud.blade.php
@@ -4,6 +4,8 @@
         <!-- Mostrar mensaje si la solicitud ya fue enviada -->
         <div class="p-6 bg-red-900 text-white text-center font-semibold mt-2">
             <p>Ya has enviado la solicitud</p>
+            <p>Revisaremos la informacion enviada</p>
+
         </div>
     @elseif (!$userStatus) <!-- Solo mostrar si el usuario está deshabilitado --> 
         
